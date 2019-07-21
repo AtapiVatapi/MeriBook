217e3d0fdf8636f0d95ed09981cfc8e8a43816b4
coderAadmi <pradyumnupadhyay2@gmail.com>
2019-07-21 11:45:53 +0530

    Firestore connected.

--- app/src/main/java/com/example/vikas/meribook/MainActivity.java
+++ app/src/main/java/com/example/vikas/meribook/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.vikas.meribook;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseApp mApp;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseApp.initializeApp(this);
+
+        mApp = FirebaseApp.getInstance();
+
+        db = FirebaseFirestore.getInstance();
+        HashMap<String,Object> map = new HashMap<String, Object>();
+        map.put("Name","Poloman");
+        db.collection("users").document("Polo").set(map);
+
     }
 }
